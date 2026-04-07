465b7fb32d851e9bd771b36cb7bcfba9f5f43de7
Enfec <enfec@example.com>
2026-04-07 15:11:24 +0530

    Fix instrumentation test with ActivityScenario

--- app/src/androidTest/java/com/example/helloworldapp/HelloWorldTest.kt
+++ app/src/androidTest/java/com/example/helloworldapp/HelloWorldTest.kt
@@ -1,8 +1,7 @@
 package com.example.helloworldapp
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,15 +12,14 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 @RunWith(AndroidJUnit4::class)
 class HelloWorldTest {
 
-    @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java)
-
     @Test
     fun testHelloWorldTextDisplayed() {
-        onView(withId(R.id.helloText))
-            .check(matches(isDisplayed()))
+        ActivityScenario.launch(MainActivity::class.java).use { scenario ->
+            onView(withId(R.id.helloText))
+                .check(matches(isDisplayed()))
 
-        onView(withId(R.id.helloText))
-            .check(matches(withText("Hello, World!")))
+            onView(withId(R.id.helloText))
+                .check(matches(withText("Hello, World!")))
+        }
     }
 }
